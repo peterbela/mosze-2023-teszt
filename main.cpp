019aac4e1fca62ef13a78e0cdc1ad383731d4d6e
peterbela <rolikpeti5@gmail.com>
2024-10-02 12:36:05 +0200

    valtoztatasok

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS];//eltérõ név
+    std::cout << '1-100 ertekek duplazasa'//pontos vesszõ nincs
+    for (int i = 0;)//rossz for
     {
         b[i] = i * 2;
     }
     for (int i = 0; i; i++)
     {
-        std::cout << "Ertek:"
-    }    
+        std::cout << "Ertek:"//pontos vesszõ, érték nincs
+    }
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag;//átlagnak nincs kezdõértéke
+    for (int i = 0; i < N_ELEMENTS, i++)//pontos vesszõ helyett sima vesszõ
     {
-        atlag += b[i]
+        atlag += b[i]//pontos vesszõ
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
